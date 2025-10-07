d4837fc6d305c4f80381a8e8f95e85cbc214b061
Ig momin Khan <mominkhan8991@gmail.com>
2025-10-07 23:26:19 +0300

    bugs

--- dist/middlewares/handleNotFoundRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const handleNotFoundRoutes = (req, res, next) => {
-    res.status(404).json({
-        success: false,
-        message: `${req.path} not found`
-    });
-};
-exports.default = handleNotFoundRoutes;
